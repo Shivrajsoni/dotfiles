61ab4a9bb59d0b77f8d2b2f90caf45a10d43dbd6
Shivraj <shivrajsoni09022005@gmail.com>
2026-04-07 00:40:36 +0530

    remove unused parts

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -31,19 +31,6 @@ config.window_padding = {
 	bottom = 0,
 }
 config.color_scheme = "Catppuccin Mocha"
---config.colors = {
---	foreground = "#CBE0F0",
---	background = "#011423",
---	cursor_bg = "#47FF9C",
---	cursor_border = "#47FF9C",
---	cursor_fg = "#011423",
---	selection_bg = "#033259",
---	selection_fg = "#CBE0F0",
---	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
---	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
---}
-
--- config.color_scheme =
 wezterm.on("gui-startup", function(cmd)
 	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
